521ff1ac81e8660009412a76848197f93e0e65a5
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:37:40 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,85 @@
+//*********** to put in commit we use // and total all statements we using contral+shift+k we use  */
+
+
+
+
+// var  a =10
+// let b = 2
+// //console.log(typeof(a))
+// //console.log(typeof(b))
+// //console.log(a+b)
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a**b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(++a)
+// console.log(--a)
+// console.log(++b)
+// console.log(--b)
+
+
+
+// var c = 10
+// var d = 9
+// c>d?
+// console.log("c is biger"):
+// console.log("d is smaller");
+
+
+// let e= 3
+// let f= 8
+// // console.log(e&f)
+
+// var result = "300"
+// console.log(result)
+// result = Number(result)
+// console.log(typeof(result))
+
+//**************************************EXPLICT CONVERSTON ****************************************************** */
+// var result
+// result = Boolean(0)     // to covert the boolean it case sentive
+// console.log(result)
+// console.log(typeof(result))
+
+
+// let result ;
+// result = String(2345);     /// to convert the string to number
+// console.log(result);
+// console.log(typeof(result))
+
+
+//  let result1 ;
+// result1 = Number("234");      // to convert  the string to number
+// console.log(result1);
+// console.log(typeof(result1))
+
+
+
+
+
+//*************  FLOAT   ***********8 */
+// var a =  '34567.0987654'
+// console.log(a)
+// console.log(parseInt(a))
+// console.log(parseFloat(a))
+// console.log(Math.floor(a))
+// console.log(typeof(a))
+
+
+// a = "ten"
+// b = Boolean(a)
+// c= Number(a)
+// console.log(a)
+// console.log(b)
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+
+
+//*********** string  */
+
+
+
