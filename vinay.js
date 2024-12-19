4a14bd15382872f9caf1806ea496cebf6def4a7d
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2024-12-19 19:38:09 +0530

    Add files via upload

---
+++ vinay.js
@@ -0,0 +1,45 @@
+
+// //  var a = 10
+// // var  b = 15
+// // var c = ("vinay")
+// // var d = 'kumar'
+
+
+// // console.log(a+b+c)
+// // console.log(d)
+
+// var a  =  3
+// var b = 'java script'
+// var c = true
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(a+b+c)
+// var d = false
+// console.log(d)
+
+// let f = 300
+// console.log(f)
+
+// const e =78
+// console.log(e)
+
+// var int = 988765
+// console.log(int)
+
+// var string_name = "vinay"
+// console.log(string_name)
+
+// var car = 'tata'
+// const price = 6098765432
+// var a,b,c
+// a = 3
+// b=5
+// c=6
+// console.log(a,b,c)
+ 
+
+// console.log(price+car)
+
+
